cb6f206409278735f9af9f035f1f2543f3814997
smile19439 <smile19439@gmail.com>
2022-04-12 01:29:18 +0800

    initialize project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs.engine({
+  defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+app.use(routes)
+
+app.listen(port, () => {
+  console.log(`App is runing on http://localhost:${port}.`)
+})
