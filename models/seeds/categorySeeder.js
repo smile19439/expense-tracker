9fe7b4fab12d8ca6c38451c97bd616c122ab52eb
smile19439 <smile19439@gmail.com>
2022-04-13 01:23:59 +0800

    add mongoose Schema and seeders

---
+++ models/seeds/categorySeeder.js
@@ -0,0 +1,33 @@
+const db = require('../../config/mongoose')
+
+const Category = require('../category')
+
+const CATEGORY = [{
+  name: '家居物業',
+  iconClassName: 'fa-solid fa-house'
+}, {
+  name: '交通出行',
+  iconClassName: 'fa-solid fa-van-shuttle',
+}, {
+  name: '休閒娛樂',
+  iconClassName: 'fa-solid fa-face-grin-beam',
+}, {
+  name: '餐飲食品',
+  iconClassName: 'fa-solid fa-utensils',
+}, {
+  name: '其他',
+  iconClassName: 'fa-solid fa-pen'
+}]
+
+db.once('open', () => {
+  return Promise.all(
+    Array.from({ length: CATEGORY.length }, (_, i) => {
+      return Category.create(CATEGORY[i])
+    })
+  )
+    .then(() => {
+      console.log('done!')
+      process.exit()
+    })
+    .catch(err => console.log(err))
+})
---
+++ models/seeds/recordSeeder.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcryptjs')
+const db = require('../../config/mongoose')
+
+const User = require('../user')
+const Category = require('../category')
+const Record = require('../record')
+
+const SEED_USER = [{
+  name: '廣志',
+  accountId: 'user1',
+  password: '1111'
+}, {
+  name: '小新',
+  accountId: 'user2',
+  password: '2222'
+}]
+
+db.once('open', () => {
+  return Promise.all(
+    Array.from({ length: SEED_USER.length }, (_, i) => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(
+          SEED_USER[i].password,
+          salt
+        ))
+
+        // 建立使用者種子資料
+        .then(hash => User.create(
+          Object.assign(SEED_USER[i], { password: hash })
+        ))
+
+        // 建立支出紀錄種子資料
+        .then(user => {
+          const userId = user._id
+          return Category.find()
+            .then(category => {
+              return Promise.all(
+                Array.from({ length: 5 }, (_, i) => Record.create({
+                  name: `name-${i}`,
+                  date: Date.now(),
+                  amount: (i + 1) * 10,
+                  userId,
+                  categoryId: category[i]._id
+                }))
+              )
+            })
+        })
+    })
+  )
+    .then(() => {
+      console.log('done!')
+      process.exit()
+    })
+    .catch(err => console.log(err))
+})
