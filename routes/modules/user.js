fcb446373decb92f7dfd1f9e2a69b1d3ef16841b
smile19439 <smile19439@gmail.com>
2022-04-14 22:07:44 +0800

    add register function

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+
+const bcrypt = require('bcryptjs')
 const passport = require('passport')
+
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -16,10 +20,25 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  console.log('register')
+  const { name, accountId, password } = req.body
+
+  User.findOne({ accountId })
+    .then(user => {
+      if (user) {
+        return res.render('register', { name, accountId, password })
+      }
+
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({ name, accountId, password: hash }))
+        .then(() => res.redirect('/user/login'))
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
 })
 
-router.get('logout', (req, res) => {
+router.get('/logout', (req, res) => {
   req.logOut()
   res.redirect('/user/login')
 })
