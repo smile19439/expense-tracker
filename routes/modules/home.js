614d32049a9c3dcdecf011807401acc3c366bbde
smile19439 <smile19439@gmail.com>
2022-06-21 21:50:26 +0800

    rearrange router of home

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,43 +10,37 @@ router.get('/', (req, res, next) => {
   const userId = req.user._id
   const categoryName = req.query.category
 
-  return Category.find() // 取得所有類別
-    .lean()
-    .sort({ _id: 'asc' })
-    .then(categories => {
+  return Category.find({ name: categoryName })
+    .then(categoryId => {
+      return Promise.all([
+        Category.find() // 取得所有類別 
+          .lean()
+          .sort({ _id: 'asc' }),
+        getTotalAmount(userId, categoryName), //取得金額總和
+        Record.find(categoryId[0] ? { userId, categoryId } : { userId }) //取得record資料
+          .populate('categoryId')
+          .lean()
+          .sort({ date: 'asc' })
+      ])
+    })
+    .then(([categories, totalAmount, records]) => {
       // 將選取的類別加入isSelected
-      categories.map(category => {
+      categories.forEach(category => {
         if (category.name === categoryName) {
           category.isSelected = true
         }
       })
-      // 選取的類別
-      const categoryId = categories.find(element => {
-        return element.name === categoryName
+
+      if (totalAmount.length === 0) {
+        const totalValue = 0
+        return res.render('index', { categories, totalValue })
+      }
+      const totalValue = totalAmount[0].total
+
+      records.map(record => {
+        record.date = moment(record.date).format("YYYY/MM/DD")
       })
-
-      Promise.resolve(getTotalAmount(userId, categoryName)) //取得金額總和
-        .then(totalAmount => {
-          if (totalAmount.length === 0) {
-            const totalValue = 0
-            return res.render('index', { categories, totalValue })
-          }
-
-          const totalValue = totalAmount[0].total
-          // 取得record資料
-          return Record.find(categoryId ? { userId, categoryId } : { userId })
-            .populate('categoryId')
-            .lean()
-            .sort({ date: 'asc' })
-            .then(records => {
-              records.map(record => {
-                record.date = moment(record.date).format("YYYY/MM/DD")
-              })
-              return res.render('index', { records, totalValue, categories })
-            })
-            .catch(error => next(error))
-        })
-        .catch(error => next(error))
+      return res.render('index', { records, totalValue, categories })
     })
     .catch(error => next(error))
 })
